e678e159ff9a92e0baf46349ec0861410120234c
Richard Rouco-Crenshaw <raroucocrenshaw@crimson.ua.edu>
2025-10-29 21:15:11 -0500

    added 10 paths. can be configured by adding the index of the path type to the list active_paths on line 60

--- simulation/multi_sensor_test.m
+++ simulation/multi_sensor_test.m
@@ -8,9 +8,11 @@ addpath('../../brew/')
 
 %% Import STL for TR obj
 
-TR = stlread("drone_shape.stl");
+TRsingle = stlread("drone_shape.stl");
 
-TR = {TR, TR};
+num_drones = 10;
+
+TR = repmat({TRsingle}, 1, num_drones);
 
 %% Setup motion model / ETT PHD
 
@@ -24,6 +26,57 @@ tf = 10;
 t = 0:dt:tf;
 
 measurements = {};
+
+g = 9.81;
+tilt_gain = 1.0;
+min_speed = 0.2;
+
+RzRyRx = @(yaw,pitch,roll) [ ...
+    cos(yaw)*cos(pitch),  cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*cos(roll),  cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll); ...
+    sin(yaw)*cos(pitch),  sin(yaw)*sin(pitch)*sin(roll)+cos(yaw)*cos(roll),  sin(yaw)*sin(pitch)*cos(roll)-cos(yaw)*sin(roll); ...
+    -sin(pitch),          cos(pitch)*sin(roll),                               cos(pitch)*cos(roll) ];
+
+% Map world accel -> small-angle pitch/roll (quad tilts to accelerate)
+% Pitch: nose-down positive ~ -atan2(a_x, g), Roll: right-wing-down ~ atan2(a_y, g)
+tiltFromAccel = @(a) deal( ...
+    atan2(norm(a(1:2)), g), ...    % total tilt magnitude (not used further)
+    -tilt_gain * atan2(a(1), g), ... % pitch
+     tilt_gain * atan2(a(2), g) );   % roll
+
+paths = cell(1, num_drones);
+
+paths{1}  = makeCirclePath([10,15, 2], 6, 0.6);
+paths{2}  = makeCirclePath([20,15, 1], 5, 0.75);
+paths{3}  = makeLemniscatePath([15,15, 2], 6, 0.5);
+paths{4}  = makeLissajousPath([15,15, 1], [7,5], [1,2], 0.5);
+paths{5}  = makeSineWeavePath([5, 5,  2], [20, 0, 0], [3, 0, 0], 0.8);
+paths{6}  = makeSineWeavePath([5, 10, 1], [0, 20, 0], [0, 3, 0], 0.8);
+paths{7}  = makeRoundedRectPath([8,8,  2], [22,22, 2], 3, 0.5);
+paths{8}  = makeEllipsePath([15,14, 1.5], [8,4], 0.6);
+paths{9}  = makeCircleWithZPath([12,18, 2], 5.5, 0.65, 1.0);
+paths{10} = makeWanderPath([22, 8, 2], [0.5, 0.4, 0.2], 0.3);
+
+
+active_paths = [1];
+
+% Subset TR and paths to the active set
+idx = active_paths(:)';        % row vector
+TR_active = TR(idx);
+paths_active = paths(idx);
+num_active = numel(idx);
+
+T = cell(num_active,1);
+
+for kAct = 1:num_active
+    [p0, v0, a0] = evalPVA(paths_active{kAct}, 0);
+    vxy = [v0(1); v0(2)];
+    if norm(vxy) < min_speed, vxy = [min_speed; 0]; end
+    yaw0 = atan2(vxy(2), vxy(1));
+    [~, pitch0, roll0] = tiltFromAccel(a0);
+    R0 = RzRyRx(yaw0, pitch0, roll0);
+    T{kAct} = [R0, p0(:); 0 0 0 1];
+end
+
 
 % initialize filter(s)
 H = [eye(3) zeros(3, 3)];
@@ -76,14 +129,6 @@ points = cell(length(sensors),length(t));
 
 %% Simulation
 
-x_true = {[25; 25; 0; -1; 0; 0],[10; 10; 0; 1; 0; 0]};
-
-T = cell(length(x_true),1);
-
-for k = 1:length(x_true)
-    T{k} = [eye(3), x_true{k}(1:3); zeros(1,3), 1];
-end
-
 est_mix = cell(length(sensors),1);
 est_colors = {'m','c'};
 
@@ -93,19 +138,18 @@ plot = 1;
 
 if plot == 0 
    for k = 1:length(t) 
-        for kk = 1:length(T)
-            x_true{kk} = target_motion.propagateState(dt,x_true{kk});
-            T{kk}(1:3,4) = x_true{kk}(1:3);
+        for kAct = 1:num_active
+            [p, v, a] = evalPVA(paths_active{kAct}, t(k));
+
+            vxy = [v(1); v(2)];
+            if norm(vxy) < min_speed, vxy = [min_speed; 0]; end
+            yaw = atan2(vxy(2), vxy(1));
+
+            [~, pitch, roll] = tiltFromAccel(a);
+            R = RzRyRx(yaw, pitch, roll);
+
+            T{kAct} = [R, p(:); 0 0 0 1];
         end
-        
-        for kk = 1:length(sensors)
-            points{kk,k} = sensors{kk}.get_points(TR,T);
-            
-            sensors{kk}.internal_tracker.predict(dt,{});
-            sensors{kk}.internal_tracker.correct(dt,points{kk,k}');
-            
-            est_mix{kk} = sensors{kk}.internal_tracker.cleanup();
-         end 
     end
 elseif plot == 1
     f = figure; 
@@ -120,14 +164,22 @@ elseif plot == 1
     view(2); hold on
 
     for k = 1:length(t) 
-        for kk = 1:length(T)
-            x_true{kk} = target_motion.propagateState(dt,x_true{kk});
-            T{kk}(1:3,4) = x_true{kk}(1:3);
+        for kAct = 1:num_active
+            [p, v, a] = evalPVA(paths_active{kAct}, t(k));
+
+            vxy = [v(1); v(2)];
+            if norm(vxy) < min_speed, vxy = [min_speed; 0]; end
+            yaw = atan2(vxy(2), vxy(1));
+
+            [~, pitch, roll] = tiltFromAccel(a);
+            R = RzRyRx(yaw, pitch, roll);
+
+            T{kAct} = [R, p(:); 0 0 0 1];
         end
         subplot(1,2,1)
         cla 
         for kk = 1:length(sensors)
-            points{kk,k} = sensors{kk}.get_points(TR,T);
+            points{kk,k} = sensors{kk}.get_points(TR_active,T);
             scatter3(points{kk,k}(:,1),points{kk,k}(:,2),points{kk,k}(:,3),6,'*'); hold on
             sensors{kk}.plot()
             
@@ -146,7 +198,7 @@ elseif plot == 1
         subplot(1,2,2)
         cla 
         for kk = 1:length(sensors)
-            points{kk,k} = sensors{kk}.get_points(TR,T);
+            points{kk,k} = sensors{kk}.get_points(TR_active,T);
             scatter3(points{kk,k}(:,1),points{kk,k}(:,2),points{kk,k}(:,3),6,'*'); hold on
             sensors{kk}.plot()
         end
@@ -184,4 +236,112 @@ for kk = 1:length(sensors)
     scatter3(points{kk,1}(:,1),points{kk,1}(:,2),points{kk,1}(:,3),6,'*'); hold on
     scatter3(points{kk,50}(:,1),points{kk,50}(:,2),points{kk,50}(:,3),6,'*'); hold on
     sensors{kk}.plot()
+end
+
+function path = makeCirclePath(c, r, w)
+    path.p = @(t) [c(1)+r*cos(w*t); c(2)+r*sin(w*t); c(3)];
+    path.v = @(t) [-r*w*sin(w*t);   r*w*cos(w*t);    0];
+    path.a = @(t) [-r*w^2*cos(w*t); -r*w^2*sin(w*t); 0];
+end
+
+function path = makeCircleWithZPath(c, r, w, wz)
+    path.p = @(t) [c(1)+r*cos(w*t); c(2)+r*sin(w*t); c(3)+0.8*sin(wz*t)];
+    path.v = @(t) [-r*w*sin(w*t);   r*w*cos(w*t);    0.8*wz*cos(wz*t)];
+    path.a = @(t) [-r*w^2*cos(w*t); -r*w^2*sin(w*t); -0.8*wz^2*sin(wz*t)];
+end
+
+function path = makeEllipsePath(c, ab, w)
+    ax = ab(1); by = ab(2);
+    path.p = @(t) [c(1)+ax*cos(w*t); c(2)+by*sin(w*t); c(3)];
+    path.v = @(t) [-ax*w*sin(w*t);   by*w*cos(w*t);    0];
+    path.a = @(t) [-ax*w^2*cos(w*t); -by*w^2*sin(w*t); 0];
+end
+
+function path = makeLemniscatePath(c, a, w)
+    % Bernoulli lemniscate (figure-eight)
+    path.p = @(t) c(:) + [a*cos(w*t)./(1+sin(w*t).^2); ...
+                          a*(sin(w*t).*cos(w*t))./(1+sin(w*t).^2); 0];
+    epsd = 1e-3;
+    path.v = @(t) (path.p(t+epsd)-path.p(t-epsd))/(2*epsd);
+    path.a = @(t) (path.v(t+epsd)-path.v(t-epsd))/(2*epsd);
+end
+
+function path = makeLissajousPath(c, ab, mn, w)
+    m = mn(1); n = mn(2);
+    ax = ab(1); by = ab(2);
+    path.p = @(t) [c(1)+ax*sin(m*w*t+pi/6); c(2)+by*sin(n*w*t); c(3)];
+    path.v = @(t) [ ax*m*w*cos(m*w*t+pi/6);  by*n*w*cos(n*w*t); 0];
+    path.a = @(t) [-ax*(m*w)^2*sin(m*w*t+pi/6); -by*(n*w)^2*sin(n*w*t); 0];
+end
+
+function path = makeSineWeavePath(c, drift, amp, w)
+    phi = pi/5; tf_guess = 10; k = 1/tf_guess;
+    path.p = @(t) [c(1)+drift(1)*k*t + amp(1)*sin(w*t); ...
+                   c(2)+drift(2)*k*t + amp(2)*sin(0.7*w*t+phi); ...
+                   c(3)+drift(3)*k*t];
+    path.v = @(t) [drift(1)*k + amp(1)*w*cos(w*t); ...
+                   drift(2)*k + amp(2)*0.7*w*cos(0.7*w*t+phi); ...
+                   drift(3)*k];
+    path.a = @(t) [-amp(1)*w^2*sin(w*t); ...
+                   -amp(2)*(0.7*w)^2*sin(0.7*w*t+phi); ...
+                    0];
+end
+
+function path = makeRoundedRectPath(minCorner, maxCorner, r, w)
+    % Rounded rectangle in XY plane, Z fixed.
+    cx = [minCorner(1)+r, maxCorner(1)-r, maxCorner(1)-r, minCorner(1)+r];
+    cy = [maxCorner(2)-r, maxCorner(2)-r, minCorner(2)+r, minCorner(2)+r];
+    z0 = minCorner(3);
+    per = 2*((maxCorner(1)-minCorner(1)-2*r) + (maxCorner(2)-minCorner(2)-2*r)) + 2*pi*r;
+    wrap = @(u) mod(u, per);
+
+    function P = xy(u)
+        Ls = [(maxCorner(1)-minCorner(1)-2*r), pi*r/2, ...
+              (maxCorner(2)-minCorner(2)-2*r), pi*r/2, ...
+              (maxCorner(1)-minCorner(1)-2*r), pi*r/2, ...
+              (maxCorner(2)-minCorner(2)-2*r), pi*r/2];
+        cum = [0, cumsum(Ls)];
+        uu = wrap(u);
+        seg = find(uu >= cum(1:end-1) & uu < cum(2:end), 1, 'first');
+        s = uu - cum(seg);
+        switch seg
+            case 1, P = [minCorner(1)+r + s, maxCorner(2)-r];
+            case 2, th = s/r; P = [cx(2)+r*cos(-pi/2+th), cy(2)+r*sin(-pi/2+th)];
+            case 3, P = [maxCorner(1)-r, maxCorner(2)-r - s];
+            case 4, th = s/r; P = [cx(3)+r*cos(0+th),    cy(3)+r*sin(0+th)];
+            case 5, P = [maxCorner(1)-r - s, minCorner(2)+r];
+            case 6, th = s/r; P = [cx(4)+r*cos(pi/2+th), cy(4)+r*sin(pi/2+th)];
+            case 7, P = [minCorner(1)+r, minCorner(2)+r + s];
+            case 8, th = s/r; P = [cx(1)+r*cos(pi+th),   cy(1)+r*sin(pi+th)];
+        end
+    end
+
+    pXY = @(t) xy(w*t);
+    path.p = @(t) local_p(pXY, t, z0);
+    epsd = 1e-3;
+    path.v = @(t) (path.p(t+epsd)-path.p(t-epsd))/(2*epsd);
+    path.a = @(t) (path.v(t+epsd)-path.v(t-epsd))/(2*epsd);
+
+    function P3 = local_p(pXY, t, z)
+        xyv = pXY(t);
+        P3 = [xyv(1); xyv(2); z];
+    end
+end
+
+function path = makeWanderPath(c, amp, w)
+    path.p = @(t) [c(1)+amp(1)*sin(w*t); ...
+                   c(2)+amp(2)*cos(0.9*w*t+0.7); ...
+                   c(3)+amp(3)*sin(1.2*w*t)];
+    path.v = @(t) [ amp(1)*w*cos(w*t); ...
+                   -amp(2)*0.9*w*sin(0.9*w*t+0.7); ...
+                    amp(3)*1.2*w*cos(1.2*w*t)];
+    path.a = @(t) [-amp(1)*w^2*sin(w*t); ...
+                   -amp(2)*(0.9*w)^2*cos(0.9*w*t+0.7); ...
+                   -amp(3)*(1.2*w)^2*sin(1.2*w*t)];
+end
+
+function [p,v,a] = evalPVA(path, t)
+    p = path.p(t);
+    v = path.v(t);
+    a = path.a(t);
 end
